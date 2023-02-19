6e8574dfe9dfe73d83070e01545765ee164c5ae7
Yuliia Kostovynska <y.kostovynska@gmail.com>
2023-02-19 21:42:49 +0100

    default value

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,29 +1,68 @@
 import axios from 'axios';
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.6.min.css';
 
-     const main = document.querySelector('main');
-     const weatherContainer = document.createElement('ul');
-     main.append(weatherContainer);
-     const weatherWidget = document.createElement('li');
-    weatherContainer.append(weatherWidget);
+let refs = {};
+
+function creatMarkupWeather() {
+  const weatherWrap = document.querySelector('.wather-wrap');
+  const weatherContainer = document.createElement('ul');
+  weatherWrap.append(weatherContainer);
+  const weatherWidget = document.createElement('li');
+  weatherContainer.append(weatherWidget);
+  weatherWidget.classList.add('weather__card');
+  weatherWidget.insertAdjacentHTML(
+    'beforeend',
+    `<div class="weather__data">
+          <div class="weather__temp">
+            <span class="weather__temp-deg"></span>
+          <span class="deg">&#176;</span>
+          </div>
+          <div class="weather__info">
+            <span class="weather__condition"></span>
+            <span class="weather__location"><svg class="location-icon" width="27" height="27"><use href="./images/icons.svg#icon-bx_search"></use></svg>
+              <p class="weather__location-place"></p> </span>
+          </div>
+        </div>
+        <img id="icon-weather">
+        <p class="weather__date">
+          <span class="weather__day-week"></span>
+          <span class="weather__month"></span>
+           </p>
+       <div class="weather__link"> <a class="weather__link-site" target="_blank" rel = ”noopener” rel = ”noreferrer”>weather for week</a></div>`
+  );
+  refs = {
+    deg: document.querySelector('.deg'),
+    iconPlace: document.querySelector('#icon-place'),
+    weatherTemp: document.querySelector('.weather__temp-deg'),
+    weatherCondition: document.querySelector('.weather__condition'),
+    weatherLocation: document.querySelector('.weather__location-place'),
+    weatherIcon: document.querySelector('#icon-weather'),
+    weatherDay: document.querySelector('.weather__day-week'),
+    weatherFullDate: document.querySelector('.weather__month'),
+    weatherLinkSite: document.querySelector('.weather__link-site'),
+  };
+  return refs;
+}
 
 // Отримання координат поточного місцязнаходження
 let latitude = +localStorage.getItem('USER_LATITUDE');
 let longitude = +localStorage.getItem('USER_LONGITUDE');
-let refs = {};
 
 function getCurrentLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(findLocation, errorHandler);
+    
   } else {
-    alert('Sorry, browser does not support geolocation!');
+    Notiflix.Notify.failure('Sorry, browser does not support geolocation!');
   }
 }
 
-function errorHandler(err) {
-  if (err.code == 1) {
-    alert('Error: Access is denied!');
-  } else if (err.code == 2) {
-    alert('Error: Position is unavailable!');
+function errorHandler(error) {
+  if (error.code == error.PERMISSION_DENIED) {
+    //   функция дефолтного запиту
+    markupWeatherCardDefault();
+    Notiflix.Notify.failure('Error: Access is denied!');
   }
 }
 
@@ -33,15 +72,16 @@ function findLocation(pos) {
   let userLongitude = crd.longitude;
   localStorage.setItem('USER_LATITUDE', userLatitude);
   localStorage.setItem('USER_LONGITUDE', userLongitude);
+  markupWeatherCard();
 }
 
-const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?';
+const BASE_URL = 'https://api.openweathermap.org/';
 const API_KEY = '26ee5cfba4c9a8162c8c1ca031ae1bc4';
 
 // Запит на сервер погоди
 async function fetchWeatherApi() {
   const res = await axios.get(
-    `${BASE_URL}lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`
+    `${BASE_URL}data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`
   );
   console.log(res.data);
   return res.data;
@@ -50,11 +90,21 @@ async function fetchWeatherApi() {
 // Запит на сервер для отриманная поточної назви міста
 async function fetchWeatherApiGeo() {
   const res = await axios.get(
-    `http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`
+    `${BASE_URL}geo/1.0/reverse?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`
   );
   console.log(res.data);
   return res.data;
 }
+
+// / Запит на сервер для отриманная погоди за замовчуванням (Київ)
+async function fetchWeatherApiDefault() {
+  const res = await axios.get(
+    `${BASE_URL}data/2.5/weather?q=London&units=metric&appid=${API_KEY}`
+  );
+  console.log(res.data);
+  return res.data;
+}
+fetchWeatherApiDefault();
 
 // Функції для отримання поточної дати/місяця/року
 let date = new Date();
@@ -84,59 +134,20 @@ function getCurrentFullDate(date) {
   return fullDate;
 }
 
-function markup() {
-  weatherWidget.insertAdjacentHTML(
-    'beforeend',
-    `<div class="weather__data">
-          <div class="weather__temp">
-            <span class="weather__temp-deg"></span>
-          <span class="deg"></span>
-          </div>
-          <div class="weather__info">
-            <span class="weather__condition"></span>
-            <span class="weather__location"><svg class="location-icon" width="27" height="27"><use id="icon-place"></use></svg>
-              <p class="weather__location-place"></p> </span>
-          </div>
-        </div>
-        <img id="icon-weather">
-        <p class="weather__date">
-          <span class="weather__day-week"></span>
-          <span class="weather__month"></span>
-           </p>
-       <div class="weather__link"> <a class="weather__link-site" target="_blank" rel = ”noopener” rel = ”noreferrer”></a></div>`
-  );
-}
 //! Єдина функція для віджету погоди
 
 function getWeatherWidget() {
-  refs = {
-     deg: document.querySelector('.deg'),
-     iconPlace:document.querySelector('#icon-place'),
-    weatherTemp: document.querySelector('.weather__temp-deg'),
-    weatherCondition: document.querySelector('.weather__condition'),
-    weatherLocation: document.querySelector('.weather__location-place'),
-    weatherIcon: document.querySelector('#icon-weather'),
-    weatherDay: document.querySelector('.weather__day-week'),
-    weatherFullDate: document.querySelector('.weather__month'),
-    weatherLinkSite: document.querySelector('.weather__link-site'),
-  };
-
   getCurrentLocation();
-  markupWeatherCard();
-  return refs;
+  
+  // markupWeatherCard();
 }
-
-markup();
 
 // Функція для дінамичного додавання даних з API до розмітки
 async function markupWeatherCard() {
   const data = await fetchWeatherApi();
-   const geo = await fetchWeatherApiGeo();
-    weatherWidget.classList.add('weather__card');
-  refs.weatherIcon.classList.add('weather__icon');
+  const geo = await fetchWeatherApiGeo();
+  creatMarkupWeather();
   refs.weatherTemp.textContent = Math.floor(data.main.temp);
-   refs.deg.insertAdjacentHTML('afterbegin', '&#176;');
-   refs.iconPlace.setAttribute('href', './images/icons.svg#icon-location');
   refs.weatherLocation.textContent = geo[0].name;
   refs.weatherCondition.textContent = data.weather[0].main;
   refs.weatherIcon.setAttribute(
@@ -145,11 +156,29 @@ async function markupWeatherCard() {
   );
   refs.weatherDay.textContent = getCurrentWeekDay(date);
   refs.weatherFullDate.textContent = getCurrentFullDate(date);
-  refs.weatherLinkSite.textContent = 'weather for week';
   refs.weatherLinkSite.setAttribute(
     'href',
     `https://www.wunderground.com/forecast/${geo[0].country}/${geo[0].name}`
   );
 }
 
-export { getWeatherWidget, markup };
+async function markupWeatherCardDefault() {
+  const data = await fetchWeatherApiDefault();
+  creatMarkupWeather();
+  refs.weatherTemp.textContent = Math.floor(data.main.temp);
+  refs.weatherLocation.textContent = data.name;
+  refs.weatherCondition.textContent = data.weather[0].main;
+  refs.weatherIcon.setAttribute(
+    'src',
+    `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+  );
+  refs.weatherDay.textContent = getCurrentWeekDay(date);
+  refs.weatherFullDate.textContent = getCurrentFullDate(date);
+  refs.weatherLinkSite.setAttribute(
+    'href',
+    `https://www.wunderground.com/forecast/gb/London`
+  );
+}
+
+
+export { getWeatherWidget };
